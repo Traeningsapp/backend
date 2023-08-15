1046779405427216ef54eee7915e01ba0395a093
franzpringle <anderst.bendtsen@gmail.com>
2023-08-15 11:33:11 +0200

    opretter workout controller med kald

---
+++ REST API/Controllers/WorkoutController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutController : ControllerBase
+    {
+        [Route("get/newworkout")]
+        [HttpGet]
+        public IActionResult GetNewWorkout()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("get/workoutfromhistory")]
+        [HttpGet]
+        public IActionResult GetWorkoutFromHistory(string userId, string workoutId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("get/workouthistory")]
+        [HttpGet]
+        public IActionResult GetWorkouthistory(string userId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("save/workout")]
+        [HttpPost]
+        public IActionResult SaveWorkout(string workoutSomJson)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+    }
+}
