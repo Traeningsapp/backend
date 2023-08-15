d5d498e74889cd8c53ecb5a8225b3b157a67cdfa
franzpringle <anderst.bendtsen@gmail.com>
2023-08-15 20:19:50 +0200

    implementerer Persistence adapter

---
+++ Persistence/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public interface IBaseRepository : IDisposable
+    {
+        IDbConnection? ÅbenConnection();
+        IDbConnection DbConnection();
+        IEnumerable<T> ExecuteStoredProcedure<T>(IDbConnection connection, string procedureNavn, object? parameters = null);
+    }
+}
