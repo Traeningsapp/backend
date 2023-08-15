6f590b405f4ef47b77b3647f653bf8f21898db96
franzpringle <anderst.bendtsen@gmail.com>
2023-08-15 11:14:51 +0200

    tilføjer exercise controller med nødvendige kald

---
+++ REST API/Controllers/ExerciseController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExerciseController : ControllerBase
+    {
+        [Route("get/musclegroups")]
+        [HttpGet]
+        public IActionResult GetMusclegroups()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("get/muscle")]
+        [HttpGet]
+        public IActionResult GetMuscles(string musclegroupId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("get/exerciselist")]
+        [HttpGet]
+        public IActionResult GetExerciselist(string muscleId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("get/exercise")]
+        [HttpGet]
+        public IActionResult GetExercise(string exerciseId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
