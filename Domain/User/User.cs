8a8434a3eadd6808d79f1fb50aaa86e88346cdd1
franzpringle <anderst.bendtsen@gmail.com>
2023-08-15 12:31:26 +0200

    tilføjer user klasse og interface

---
+++ Domain/User/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.User
+{
+    public class User : IUser
+    {
+        public User(string id)
+        {
+            _id = id;
+        }
+        private string _id;
+
+        public string Id
+        {
+            get => _id; set => _id = value;
+        }
+    }
+}
