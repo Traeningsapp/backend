2225ff6d354f383df4d4bd86d5834b7f6b16e158
Alligo324 <54527833+Alligo324@users.noreply.github.com>
2023-09-04 07:10:47 +0200

    Added-Patch-Workout
    Added splitType to workout for retrieval.
    Added deleteWorkout to API

--- Application/Ports/Incoming/IWorkoutUseCase.cs
+++ Application/Ports/Incoming/IWorkoutUseCase.cs
@@ -5,8 +5,9 @@ namespace Application.Ports.Incoming
     public interface IWorkoutUseCase
     {
         IWorkout GenerateNewWorkout(int split_id, string userId);
-        int SaveWorkout(string userId, string workoutAsJson);
+        int SaveWorkout(string userId, string workoutAsJsonm, string splitType);
         List<IWorkout> GetWorkoutHistory(string userId);
         IWorkout StartWorkoutFromHistory(string userId, int workoutId);
+        void deleteWorkout(int workoutId);
     }
 }
